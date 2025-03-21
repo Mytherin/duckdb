f2872985ac2331420b1696e07d9fec28ca35e60d
Mytherin <mark.raasveldt@gmail.com>
2025-03-21 12:46:11 +0100

    Grumble grumble

--- src/common/string_util.cpp
+++ src/common/string_util.cpp
@@ -8,8 +8,8 @@
 #include "duckdb/common/exception/parser_exception.hpp"
 #include "duckdb/common/random_engine.hpp"
 #include "duckdb/common/numeric_utils.hpp"
-#include "duckdb/common/hugeint.hpp"
-#include "duckdb/common/uhugeint.hpp"
+#include "duckdb/common/types/hugeint.hpp"
+#include "duckdb/common/types/uhugeint.hpp"
 #include "jaro_winkler.hpp"
 #include "utf8proc_wrapper.hpp"
 
